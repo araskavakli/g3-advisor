f2d9cffe0cabe90044b2d0b4dc1871a3adee9685
Aras Kavakli <araskavakli@gmail.com>
2020-04-10 18:31:44 +0200

    docs fodler created for github pages

---
+++ docs/assets/cityImageUrls.ts
@@ -0,0 +1,7 @@
+export class CityImageUrls {
+  urlList = [
+    'https://images.unsplash.com/photo-1540646794357-6cbbd6f3501e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1560969184-10fe8719e047?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+    'https://images.unsplash.com/photo-1422360902398-0a91ff2c1a1f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+  ];
+}
